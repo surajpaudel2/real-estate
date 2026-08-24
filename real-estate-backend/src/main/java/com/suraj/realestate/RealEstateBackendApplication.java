6c7f009bf23ba5409a6ca172e44be343662b8c2b
Suraj Paudel <Surajpaudel4833@gmail.com>
2026-08-24 13:14:32 +1000

    Improved by .env way of giving for the secuirty

--- real-estate-backend/src/main/java/com/suraj/realestate/RealEstateBackendApplication.java
+++ real-estate-backend/src/main/java/com/suraj/realestate/RealEstateBackendApplication.java
@@ -1,5 +1,6 @@
 package com.suraj.realestate;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
